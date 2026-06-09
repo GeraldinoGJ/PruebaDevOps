bcaf210fb87fbcb0c0b865906a1c2c6dc590e717
kimetsu23 <jeremygeraldo23@gmail.com>
2026-06-09 16:28:11 -0400

    Fix: Index.js

--- Index.js
+++ Index.js
@@ -1,3 +1,3 @@
-Console.log("Hello World!");
-Console.log("Welcome to JavaScript programming.");
-Console.log("Send alert to ntfy.sh/devops-itla");
+console.log("Hello World!");
+console.log("Welcome to JavaScript programming.");
+console.log("Send alert to ntfy.sh/devops-itla");
